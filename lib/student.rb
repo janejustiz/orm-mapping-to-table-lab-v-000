1b190736f89f33ceeeb9e339659fbbdd75c9a150
Jane Justiz <janejustiz@gmail.com>
2019-07-31 18:01:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,13 @@ class Student
   end
 
   def self.create_table
+    sql = <- SQL
+    CREATE TABLE student(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    );
+    SQL
   end
 
   def self.drop_table
